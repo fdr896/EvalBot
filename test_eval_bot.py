9971d47730265056cecadf14478167366f5c3af6
Fedor Osetrov <fiosetrova@gmail.com>
2021-12-25 18:27:35 +0300

    Add CI for tests

---
+++ test_eval_bot.py
@@ -0,0 +1,17 @@
+def echo_all(message):
+    """Evals received message"""
+
+    try:
+        result = eval(message) # pylint: disable=eval-used
+        return "Expression result is: " + str(result)
+    except SyntaxError:
+        return "Your message does not satisfy python syntax!"
+    except: # pylint: disable=bare-except
+        return "Your message has an error in runtime or in 'compilation'!"
+
+def test_echo_all():
+	prefix = "Expression result is: "
+
+	assert echo_all("1 + 3") == prefix + "4"
+	assert echo_all("test string") == "Your message does not satisfy python syntax!"
+	assert echo_all("1 // 0") == "Your message has an error in runtime or in 'compilation'!"
