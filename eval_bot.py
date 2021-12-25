0bb32611b7edf6b718cdec9a14145a2ececb05c7
Fedor Osetrov <fiosetrova@gmail.com>
2021-12-25 17:38:44 +0300

    Refactor code for pylint

--- eval_bot.py
+++ eval_bot.py
@@ -1,14 +1,27 @@
-import os, telebot
+"""Telegram bot who evals your messages"""
+
+import os
+import telebot
 
 TOKEN = os.environ["TOKEN"]
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start'])
 def send_hello(message):
-	bot.reply_to(message, "Hello man")
+    """Places greeting message"""
+
+    bot.reply_to(message, "Hello man")
 
 @bot.message_handler(func = lambda m: True)
 def echo_all(message):
-	bot.reply_to(message, "Expression result is: " + str(eval(message.text)))
+    """Evals received message"""
+
+    try:
+        result = eval(message.text) # pylint: disable=eval-used
+        bot.reply_to(message, "Expression result is: " + str(result))
+    except SyntaxError:
+        bot.reply_to(message, "Your message does not satisfy python syntax!")
+    except: # pylint: disable=bare-except
+        bot.reply_to(message, "Your message has an error in runtime or in 'compilation'!")
 
 bot.infinity_polling()
