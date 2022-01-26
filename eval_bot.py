2da45615fd6ebce41c5c8035b73716ccd39dae1d
Fedor Osetrov <fiosetrova@gmail.com>
2022-01-27 00:43:41 +0300

    Fix linter bugs

--- eval_bot.py
+++ eval_bot.py
@@ -2,8 +2,8 @@
 
 import os
 import math # pylint: disable=unused-import
-import numpy as np
-import sklearn
+import numpy as np # pylint: disable=unused-import
+import sklearn # pylint: disable=unused-import
 import telebot
 
 TOKEN = os.environ["TOKEN"]
